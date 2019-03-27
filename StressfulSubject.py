0195a5a602434dea6cbe98079248b44e8acf9b93
Nadya <n.perehodko@yandex.com>
2019-03-27 14:17:34 +0300

    Add files via upload

--- StressfulSubject.py
+++ StressfulSubject.py
@@ -1,10 +1,9 @@
 '''
 Задание:
-The function should recognise if a subject line is stressful. A stressful subject line means
-that all letters are in uppercase, and/or ends by at least 3 exclamation marks, and/or
-contains at least one of the following “red” words: "help", "asap", "urgent".
-Any of those "red" words can be spelled in different ways - "HELP", "help", "HeLp",
-"H!E!L!P!", "H-E-L-P", even in a very loooong way "HHHEEEEEEEEELLP"
+The function should recognise if a subject line is stressful. A stressful subject line means that all letters
+are in uppercase, and/or ends by at least 3 exclamation marks, and/or contains at least one of the following
+“red” words: "help", "asap", "urgent". Any of those "red" words can be spelled in different ways - "HELP",
+"help", "HeLp", "H!E!L!P!", "H-E-L-P", even in a very loooong way "HHHEEEEEEEEELLP"
 
 Input: Subject line as a string.
 Output: Boolean.
@@ -13,27 +12,27 @@ Precondition: Subject can be up to 100 letters
 
 
 def is_stressful(subj):
-    # список слов, на которые надо реагировать
+    # Список слов, на которые надо реагировать
     stressful_list = ["help", "asap", "urgent"]
 
-    # результат, котрый возвращает функция
+    # Результат, котрый возвращает функция
     result = False
 
-    # длина полученного слова
+    # Длина полученного слова
     len_subj = len(subj)
 
-    # если последние три символа "!!!" значит письмо срочное
+    # Если последние три символа "!!!", значит письмо срочное
     if "!!!" in subj[(len_subj) - 3:]:
         result = True
-    # если весь текст в верзнем регистре, значит письмо тоже срочное
+    # Если весь текст в верхнем регистре, значит письмо тоже срочное
     elif subj.isupper() == True:
         result = True
-    # если первые два условия не про нас, то идем дальше
+    # Если первые два условия не про нас, то идем дальше
     else:
-        # преобразакм строку в нижний регистр
+        # Преобразуем строку в нижний регистр
         lowercase_subj = subj.lower()
 
-        # убираем лишние символы из строки
+        # Убираем лишние символы из строки
         clean_subj1 = lowercase_subj.replace("!", "")
         clean_subj2 = clean_subj1.replace("-", "")
         clean_subj3 = clean_subj2.replace(".", "")
@@ -41,14 +40,13 @@ def is_stressful(subj):
         # Отладочная информация
         # print(clean_subj2)
 
-        # добавляю один символ в конец строки
+        # Добавляю один символ в конец строки
         clean_subj = clean_subj3 + "a"
 
-        # сюда буду записывать слово без повторений
+        # Сюда буду записывать слово без повторений символов
         new_word = []
 
-        # если текущий и следующий за ним символ не совпадает,
-        # то записываю его в new_word
+        # Если текущий и следующий за ним символ не совпадает, то записываю его в new_word
         for j in range(0, len(clean_subj) - 1, 1):
             # print(clean_subj[j])
             if clean_subj[j] == clean_subj[j + 1]:
@@ -59,14 +57,14 @@ def is_stressful(subj):
         # Преобразаую список в строку
         my_string = "".join(new_word)
 
-        # Проверяю есть ли одно из стресс-слов из списка stressful_list
-        # в строке my_string
+        # Проверяю есть ли одно из стресс-слов из списка stressful_list в моей строке
         for i in stressful_list:
             if i in my_string:
                 result = True
             else:
                 pass
 
+    # Возвращаю результат
     if result == True:
         return True
     else:
