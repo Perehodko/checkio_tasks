ade8f15d19acf98ed572357200cb987a8e0156de
Nadya <n.perehodko@yandex.com>
2019-03-29 14:27:18 +0300

    Add files via upload

--- MostWantedLetter.py
+++ MostWantedLetter.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-Задание
+Задание:
 You are given a text, which contains different english letters and punctuation symbols. You should find the most
 frequent letter in the text. The letter returned must be in lower case. While checking for the most wanted letter,
 casing does not matter, so for the purpose of your search, "A" == "a". Make sure you do not count punctuation symbols,
